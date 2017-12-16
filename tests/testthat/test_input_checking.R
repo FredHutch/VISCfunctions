0be2d4a85f153174dbd0cf28579dffbb02f9de4a
wfulp <wfulp@scharp.org>
2017-12-15 17:10:34 -0800

    Changed expect_error = NA to expect_null when not expecting errors

--- tests/testthat/test_input_checking.R
+++ tests/testthat/test_input_checking.R
@@ -2,10 +2,10 @@ context("input_checking")
 
 # test .check_numeric_input
 test_that(".check_numeric_input error-free when expected", {
-  expect_error(.check_numeric_input(1:10), NA)
-  expect_error(.check_numeric_input(c(1:10,NA,1:20,NA)), NA)
+  expect_null(.check_numeric_input(1:10))
+  expect_null(.check_numeric_input(c(1:10,NA,1:20,NA)))
   my_vals <- c(1,5,2,NA)
-  expect_error(.check_numeric_input(my_vals), NA)
+  expect_null(.check_numeric_input(my_vals))
 })
 
 test_that(".check_numeric_input throws appropriate errors", {
@@ -25,13 +25,13 @@ test_that(".check_numeric_input throws appropriate errors", {
 
 # test .check_binary_input
 test_that(".check_binary_input error-free when expected", {
-  expect_error(.check_binary_input(c('a','b')), NA)
-  expect_error(.check_binary_input(1:2), NA)
-  expect_error(.check_binary_input(5), NA)
-  expect_error(.check_binary_input(factor(rep(1:2,10))), NA)
-  expect_error(.check_binary_input(c(F,T,T,F)), NA)
+  expect_null(.check_binary_input(c('a','b')))
+  expect_null(.check_binary_input(1:2))
+  expect_null(.check_binary_input(5))
+  expect_null(.check_binary_input(factor(rep(1:2,10))))
+  expect_null(.check_binary_input(c(F,T,T,F)))
   my_vals <- c(1,10,1,1,NA,10)
-  expect_error(.check_binary_input(my_vals), NA)
+  expect_null(.check_binary_input(my_vals))
 })
 
 test_that(".check_binary_input throws appropriate errors", {
@@ -45,10 +45,10 @@ test_that(".check_binary_input throws appropriate errors", {
 
 # test .check_response_input
 test_that(".check_response_input error-free when expected", {
-  expect_error(.check_response_input(c(0,0,0,1,1,0,0,1)), NA)
-  expect_error(.check_response_input(c(T,T,F,F,T,F)), NA)
+  expect_null(.check_response_input(c(0,0,0,1,1,0,0,1)))
+  expect_null(.check_response_input(c(T,T,F,F,T,F)))
   my_vals <- c(1,0,1,1,NA,0)
-  expect_error(.check_response_input(my_vals), NA)
+  expect_null(.check_response_input(my_vals))
 })
 
 test_that(".check_response_input throws appropriate errors", {
