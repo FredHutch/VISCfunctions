2124aa239906cc0a5e2bec51ee12d461a53cf1a2
wfulp <wfulp@scharp.org>
2017-12-28 14:56:18 -0800

    Added quotes around the params in the error messages

--- tests/testthat/test_input_checking.R
+++ tests/testthat/test_input_checking.R
@@ -10,19 +10,19 @@ test_that(".check_numeric_input error-free when expected", {
 
 test_that(".check_numeric_input throws appropriate errors", {
   my_matrix <- matrix(1:10,nrow = 2)
-  expect_error(.check_numeric_input(my_matrix), 'my_matrix must be a vector \\(one-dimensional object\\)')
+  expect_error(.check_numeric_input(my_matrix), '"my_matrix" must be a vector \\(one-dimensional object\\)')
 
-  expect_error(.check_numeric_input(numeric(0)), 'numeric\\(0\\) length must be > 0')
+  expect_error(.check_numeric_input(numeric(0)), '"numeric\\(0\\)" length must be > 0')
 
-  expect_error(.check_numeric_input(c(NA,NA,NA)), 'c\\(NA, NA, NA\\) must have at least one non-NA value')
+  expect_error(.check_numeric_input(c(NA,NA,NA)), '"c\\(NA, NA, NA\\)" must have at least one non-NA value')
 
-  expect_error(.check_numeric_input(letters[1:5]), 'letters\\[1:5\\] must be a numeric vector')
+  expect_error(.check_numeric_input(letters[1:5]), '"letters\\[1:5\\]" must be a numeric vector')
   my_letters <- letters[1:10]
-  expect_error(.check_numeric_input(my_letters), 'my_letters must be a numeric vector')
+  expect_error(.check_numeric_input(my_letters), '"my_letters" must be a numeric vector')
 
-  expect_error(.check_numeric_input(1:10, lower_bound = 3), '1\\:10 must be greater than 3')
+  expect_error(.check_numeric_input(1:10, lower_bound = 3), '"1\\:10" must be greater than 3')
 
-  expect_error(.check_numeric_input(1:10, lower_bound = 0, upper_bound = 5), '1\\:10 must be less than 5')
+  expect_error(.check_numeric_input(1:10, lower_bound = 0, upper_bound = 5), '"1\\:10" must be less than 5')
 })
 
 
@@ -39,13 +39,13 @@ test_that(".check_binary_input error-free when expected", {
 
 test_that(".check_binary_input throws appropriate errors", {
   my_matrix <- matrix(rep(c('a','b'),5),nrow = 2)
-  expect_error(.check_binary_input(my_matrix), 'my_matrix must be a vector \\(one-dimensional object\\)')
-  expect_error(.check_binary_input(c(1:2,NA), paired = TRUE), 'When "paired" = TRUE c\\(1:2, NA\\) cannot have missing values')
-  expect_error(.check_binary_input(numeric(0)), 'numeric\\(0\\) length must be > 0')
-  expect_error(.check_binary_input(c(NA,NA,NA)), 'c\\(NA, NA, NA\\) must have at least one non-NA value')
-  expect_error(.check_binary_input(1:10), '1:10 cannot have more than 2 distinct values')
-  expect_error(.check_binary_input(1, paired = TRUE), 'When "paired" = TRUE 1 must have exactly 2 distinct values')
-  expect_error(.check_binary_input(c(1,1,2), paired = TRUE), 'When "paired" = TRUE c\\(1, 1, 2\\) must have the same number of samples for each level')
+  expect_error(.check_binary_input(my_matrix), '"my_matrix" must be a vector \\(one-dimensional object\\)')
+  expect_error(.check_binary_input(c(1:2,NA), paired = TRUE), 'When "paired" = TRUE "c\\(1:2, NA\\)" cannot have missing values')
+  expect_error(.check_binary_input(numeric(0)), '"numeric\\(0\\)" length must be > 0')
+  expect_error(.check_binary_input(c(NA,NA,NA)), '"c\\(NA, NA, NA\\)" must have at least one non-NA value')
+  expect_error(.check_binary_input(1:10), '"1:10" cannot have more than 2 distinct values')
+  expect_error(.check_binary_input(1, paired = TRUE), 'When "paired" = TRUE "1" must have exactly 2 distinct values')
+  expect_error(.check_binary_input(c(1,1,2), paired = TRUE), 'When "paired" = TRUE "c\\(1, 1, 2\\)" must have the same number of samples for each level')
 })
 
 
@@ -59,17 +59,17 @@ test_that(".check_response_input error-free when expected", {
 
 test_that(".check_response_input throws appropriate errors", {
   my_matrix <- matrix(rep(0:1,5),nrow = 2)
-  expect_error(.check_response_input(my_matrix), 'my_matrix must be a vector \\(one-dimensional object\\)')
+  expect_error(.check_response_input(my_matrix), '"my_matrix" must be a vector \\(one-dimensional object\\)')
 
-  expect_error(.check_response_input(numeric(0)), 'numeric\\(0\\) length must be > 0')
+  expect_error(.check_response_input(numeric(0)), '"numeric\\(0\\)" length must be > 0')
 
-  expect_error(.check_response_input(c(NA,NA,NA)), 'c\\(NA, NA, NA\\) must have at least one non-NA value')
+  expect_error(.check_response_input(c(NA,NA,NA)), '"c\\(NA, NA, NA\\)" must have at least one non-NA value')
 
-  expect_error(.check_response_input(1:10), '1:10 must be a numeric vector containing only 0/1 values or a logical vector containing only T/F values')
+  expect_error(.check_response_input(1:10), '"1:10" must be a numeric vector containing only 0/1 values or a logical vector containing only T/F values')
   my_letters <- c('a','b')
-  expect_error(.check_response_input(my_letters), 'my_letters must be a numeric vector containing only 0/1 values or a logical vector containing only T/F values')
+  expect_error(.check_response_input(my_letters), '"my_letters" must be a numeric vector containing only 0/1 values or a logical vector containing only T/F values')
   my_letters <- c('T','F')
-  expect_error(.check_response_input(my_letters), 'my_letters must be a numeric vector containing only 0/1 values or a logical vector containing only T/F values')
+  expect_error(.check_response_input(my_letters), '"my_letters" must be a numeric vector containing only 0/1 values or a logical vector containing only T/F values')
 })
 
 
