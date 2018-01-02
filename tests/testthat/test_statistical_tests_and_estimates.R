011b749879d2254dc6f61e7522d93d1a347b8df1
wfulp <wfulp@scharp.org>
2018-01-02 09:28:02 -0800

    Fixed some quote testing

--- tests/testthat/test_statistical_tests_and_estimates.R
+++ tests/testthat/test_statistical_tests_and_estimates.R
@@ -39,23 +39,23 @@ test_that("two_samp_cont_test testing", {
   expect_error(two_samp_cont_test(x = x[-1], y = y), '"x" and "y" must be the same length')
 
   #Testing paired errors
-  expect_error(two_samp_cont_test(x = x, y = y, method = 't', paired = TRUE), 'When "paired" = TRUE y cannot have missing values')
-  expect_error(two_samp_cont_test(x = x[-(11:13)], y = y[-(11:13)], method = 't', paired = TRUE), 'When "paired" = TRUE y must have the same number of samples for each level')
+  expect_error(two_samp_cont_test(x = x, y = y, method = 't', paired = TRUE), 'When "paired" = TRUE "y" cannot have missing values')
+  expect_error(two_samp_cont_test(x = x[-(11:13)], y = y[-(11:13)], method = 't', paired = TRUE), 'When "paired" = TRUE "y" must have the same number of samples for each level')
 
   #Testing case where no non-missing pairs
   expect_equal(object = two_samp_cont_test(x = c(rep(1,20),rep(NA,20)), y = c(rep(NA,20),rep(1,20))), expected = NA)
   expect_message(object = two_samp_cont_test(x = c(rep(1,20),rep(NA,20)), y = c(rep(NA,20),rep(1,20)), verbose = T),
-                 regexp = 'There are no observations with non-mising values of both "x" and "y", so p=NA returned')
+                 regexp = 'There are no observations with non-missing values of both "x" and "y", so p=NA returned')
 
   #Testing case where all x have same value
   expect_equal(object = two_samp_cont_test(x = rep(1,22), y = y), expected = 1)
   expect_message(object = two_samp_cont_test(x = rep(1,22), y = y, verbose = T),
-                 regexp = '"x" only has 1 distinct value when considering non-missing values of y, so p=1 returned')
+                 regexp = '"x" only has 1 distinct value when considering non-missing values of "y", so p=1 returned')
 
   #Testing case where all y have same value
   expect_equal(object = two_samp_cont_test(x = x, y = rep(1,22)), expected = NA)
   expect_message(object = two_samp_cont_test(x = x, y = rep(1,22), verbose = T),
-                 regexp = '"y" only has 1 level when considering non-missing values of x, so p=NA returned')
+                 regexp = '"y" only has 1 level when considering non-missing values of "x", so p=NA returned')
 })
 
 test_that("two_samp_cont_test throwing internal input checking errors", {
@@ -65,14 +65,14 @@ test_that("two_samp_cont_test throwing internal input checking errors", {
   my_matrix <- matrix(1:10,nrow = 2)
 
   #Checking x
-  expect_error(two_samp_cont_test(my_matrix, y = y), 'x must be a vector \\(one-dimensional object\\)')
-  expect_error(two_samp_cont_test(x = numeric(0), y = y), 'x length must be > 0')
-  expect_error(two_samp_cont_test(c(NA,NA,NA), y), 'x must have at least one non-NA value')
-  expect_error(two_samp_cont_test(letters[1:5],y), 'x must be a numeric vector')
+  expect_error(two_samp_cont_test(my_matrix, y = y), '"x" must be a vector \\(one-dimensional object\\)')
+  expect_error(two_samp_cont_test(x = numeric(0), y = y), '"x" length must be > 0')
+  expect_error(two_samp_cont_test(c(NA,NA,NA), y), '"x" must have at least one non-NA value')
+  expect_error(two_samp_cont_test(letters[1:5],y), '"x" must be a numeric vector')
 
   #Checking y
-  expect_error(two_samp_cont_test(x, my_matrix), 'y must be a vector \\(one-dimensional object\\)')
-  expect_error(two_samp_cont_test(x,numeric(0)), 'y length must be > 0')
-  expect_error(two_samp_cont_test(x,c(NA,NA,NA)), 'y must have at least one non-NA value')
-  expect_error(two_samp_cont_test(x,1:10), 'y cannot have more than 2 distinct values')
+  expect_error(two_samp_cont_test(x, my_matrix), '"y" must be a vector \\(one-dimensional object\\)')
+  expect_error(two_samp_cont_test(x,numeric(0)), '"y" length must be > 0')
+  expect_error(two_samp_cont_test(x,c(NA,NA,NA)), '"y" must have at least one non-NA value')
+  expect_error(two_samp_cont_test(x,1:10), '"y" cannot have more than 2 distinct values')
 })
