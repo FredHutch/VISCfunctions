8a529a0a5f10deab06e10fa4670c0baa3fd15814
wfulp <wfulp@scharp.org>
2018-04-23 13:08:13 -0700

    Documentation typo for round lower bound

--- R/statistical_tests_and_estimates.R
+++ R/statistical_tests_and_estimates.R
@@ -3,7 +3,7 @@
 #' round_away_0 takes a numeric vector, rounds them to a specified digit amount using the round away from 0 method for ties (i.e. 1.5). This is the SAS method for rounding.
 #'
 #' @param x numeric vector (can include NA values).
-#' @param rounding_digits positive integer of length 1 between 1 and 14, giving the amount of digits to round to.
+#' @param rounding_digits positive integer of length 1 between 0 and 14, giving the amount of digits to round to.
 #' @param verbose a logical variable indicating if warnings and messages should be displayed.
 #' @return numeric vector of rounded values.
 #' @details
