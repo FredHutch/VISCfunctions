15716dc7543e4f891620778ff63f2a3b24b26127
wfulp <wfulp@scharp.org>
2018-06-04 10:59:30 -0700

    .rm_na_and_check: Adding in x_type param

--- R/data_processing_functions.R
+++ R/data_processing_functions.R
@@ -2,7 +2,8 @@
 #'
 #' @param x numeric vector (can include NA values).
 #' @param y numeric vector (can include NA values).
-#' @param y_type is y a binary/group variable or continuous variable. This will impact if a p-value of NA or 1 is returned when y has only 1 distinct value.
+#' @param x_type is x a binary/group variable or continuous variable (default). This will impact if a p-value of NA or 1 is returned when x has only 1 distinct value.
+#' @param y_type is y a binary/group variable (default) or continuous variable. This will impact if a p-value of NA or 1 is returned when y has only 1 distinct value.
 #' @param verbose a logical variable indicating if warnings and messages should be displayed.
 #'
 #' @return A data.frame with all completely NA rows removed or a NA or 1 pvalue if no complete cases or only one distinct value.
@@ -15,9 +16,10 @@
 #' .rm_na_and_check(x, y_cont, y_type = 'continuous')
 #'
 
-.rm_na_and_check = function(x, y, y_type = c('binary', 'continuous'), verbose = FALSE){
+.rm_na_and_check = function(x, y, x_type = c('continuous', 'binary'), y_type = c('binary', 'continuous'), verbose = FALSE){
+  x_type <- match.arg(x_type)
+  if (x_type == 'binary') .check_binary_input(x) else .check_numeric_input(x)
   y_type <- match.arg(y_type)
-  .check_numeric_input(x)
   if (y_type == 'binary') .check_binary_input(y) else .check_numeric_input(y)
 
   if (length(x) != length(y)) stop('"x" and "y" must be the same length')
