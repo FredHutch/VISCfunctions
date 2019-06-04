72722f5194cefd73d6b9294c74d8469714ba5c50
wfulp <wfulp@fredhutch.org>
2019-06-04 14:14:41 -0700

    Adding in more test cases for reproducibility tables

--- tests/testthat/test_reproducibility_tables.R
+++ tests/testthat/test_reproducibility_tables.R
@@ -21,5 +21,26 @@ test_that("get_session_info() testing", {
   dim_expected <- ifelse(any(colnames(temp_session_info$packages_table) == "data.version"), 5, 4)
   expect_equal(object = ncol(temp_session_info$packages_table), expected = dim_expected)
 
+
+  ## testing some outputs from sessioninfo::session_info()
+  expected_session_info <- sessioninfo::session_info()
+
+  # Comparing platform
+  expected_platform <- data.frame(
+    name = names(expected_session_info$platform),
+    value = matrix(unlist(expected_session_info$platform), nrow = length(expected_session_info$platform)),
+    stringsAsFactors = FALSE)
+  expect_equal(object = temp_session_info$platform_table[match(expected_platform$name, temp_session_info$platform_table$name), ], expected = expected_platform)
+
+  # Comparing packages
+  expected_packages <- expected_session_info$packages[expected_session_info$packages$attached,]
+  expected_packages <- data.frame(package = expected_packages$package,
+                                 version = expected_packages$loadedversion,
+                                 date = expected_packages$date,
+                                 source = expected_packages$source,
+                                 stringsAsFactors = FALSE)
+
+  expect_equal(object = temp_session_info$packages_table, expected = expected_packages)
+
 })
 
