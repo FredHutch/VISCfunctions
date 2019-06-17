49a0f88cef0e08cd3c2b2d44898ffe7664040c0f
wfulp <wfulp@fredhutch.org>
2019-06-17 15:21:19 -0700

    Adding test cases for pairwise_test_bin

--- tests/testthat/test_pairwise_comparisons.R
+++ tests/testthat/test_pairwise_comparisons.R
@@ -1,8 +1,8 @@
 context("pairwise_comparisons")
 
 
-# test paste_tbl_grp. Using paste_tbl_grp and pairwise_test_cont in testing since these functions are testing elsewhere
-test_that("pairwise_comparisons testing two groups", {
+# test pairwise_test_cont. Using paste_tbl_grp and two_samp_cont_test in testing since these functions are testing elsewhere
+test_that("pairwise_comparisons_bin testing two groups", {
   set.seed(243542534)
   x = c(NA, rnorm(25, 0, 1), rnorm(25, 1, 1),NA)
   group = c(rep('a', 26),rep('b', 26))
@@ -153,7 +153,7 @@ test_that("Integration with dplyr and data.table is equivalent", {
 
 
 
-# test paste_tbl_grp. Using paste_tbl_grp and pairwise_test_cont in testing since these functions are testing elsewhere
+# test pairwise_test_cont. Using paste_tbl_grp and two_samp_cont_test in testing since these functions are testing elsewhere
 test_that("pairwise_comparisons testing multiple groups", {
 
   test_single_comp <- function(x, group, Group1, Group2) {
@@ -302,3 +302,199 @@ test_that("Paired results with test data", {
   expect_equal(group_testing_tibble_less$MagnitudeTest, group_testing_tibble_less$est_less)
 
 })
+
+
+
+
+
+
+
+# test pairwise_test_bin.
+#Using paste_tbl_grp and two_samp_bin_test in testing since these functions are testing elsewhere
+test_that("pairwise_comparisons_bin testing two groups", {
+  set.seed(243542534)
+  x = c(NA,sample(0:1,25,replace = TRUE, prob = c(.75,.25)),NA,
+        sample(0:1,25,replace = TRUE, prob = c(.25,.75)))
+  group = c(rep('a', 26),rep('b', 26))
+  id = c(1:26, 1:26)
+  test_data <- tibble(x, group, id)
+
+  testing_stats_pre <- test_data %>%
+    filter(!is.na(x)) %>%
+    group_by(group) %>%
+    group_modify( ~ wilson_ci(.$x, .95)) %>%
+    mutate(rr = stat_paste(mean * 100, lower * 100, upper * 100, digits = 1, suffix = '%'))
+
+  testing_stats <- bind_cols(
+    testing_stats_pre %>% filter(group == 'a') %>% select(Group1 = group, Group1_rr = rr),
+    testing_stats_pre %>% filter(group == 'b') %>% select(Group2 = group, Group2_rr = rr)
+    )
+
+
+  # testing multiple methods
+  test_pasting <- paste_tbl_grp(data = testing_stats)
+  names(test_pasting) <- c('Comparison', 'ResponseStats')
+
+  purrr::walk(c("barnard", "fisher", "chi.sq"), function(method_in){
+    testing_results <- data.frame(
+      test_pasting,
+      ResponseTest  = two_samp_bin_test(x = x, y = group, method = method_in),
+      PerfectSeperation = ifelse(diff(testing_stats_pre$mean) == 1, TRUE, FALSE)
+    )
+    expect_equal(object = pairwise_test_bin(x = x, group = group, method = method_in),
+                 expected = testing_results)
+  }
+  )
+
+  # Digits to 3
+  testing_stats_pre_3digits <- test_data %>%
+    filter(!is.na(x)) %>%
+    group_by(group) %>%
+    group_modify( ~ wilson_ci(.$x, .95)) %>%
+    mutate(rr = stat_paste(mean * 100, lower * 100, upper * 100, digits = 3, suffix = '%'))
+
+  testing_stats_3digits <- bind_cols(
+    testing_stats_pre_3digits %>% filter(group == 'a') %>% select(Group1 = group, Group1_rr = rr),
+    testing_stats_pre_3digits %>% filter(group == 'b') %>% select(Group2 = group, Group2_rr = rr)
+  )
+
+  test_pasting <- paste_tbl_grp(data = testing_stats_3digits)
+  names(test_pasting) <- c('Comparison', 'ResponseStats')
+  testing_results <- data.frame(
+    test_pasting,
+    ResponseTest  = two_samp_bin_test(x = x, y = group),
+    PerfectSeperation = ifelse(diff(testing_stats_pre_3digits$mean) == 1, TRUE, FALSE)
+  )
+  expect_equal(object = pairwise_test_bin(x = x, group = group, digits = 3),
+               expected = testing_results)
+
+
+  # one-sided test comparison
+  test_pasting <- paste_tbl_grp(data = testing_stats, alternative = 'less')
+  names(test_pasting) <- c('Comparison', 'ResponseStats')
+  testing_results <- data.frame(
+    test_pasting,
+    # Need reverse testing direction
+    ResponseTest  = two_samp_bin_test(x = x, y = group, alternative = 'greater'),
+    PerfectSeperation = ifelse(diff(testing_stats_pre$mean) == 1, TRUE, FALSE)
+  )
+  expect_equal(object = pairwise_test_bin(x = x, group = group, alternative = 'less'),
+               expected = testing_results)
+
+
+
+  # sorted group greater than comparison
+  test_pasting <- paste_tbl_grp(data = testing_stats, alternative = 'greater',
+                                first_name = 'Group2', second_name = 'Group1')
+  names(test_pasting) <- c('Comparison', 'ResponseStats')
+  testing_results <- data.frame(
+    test_pasting,
+    # Need reverse testing direction
+    ResponseTest  = two_samp_bin_test(x = x, y = factor(group, levels = c('b','a')),
+                                      alternative = 'less'),
+    PerfectSeperation = ifelse(diff(testing_stats_pre$mean) == 1, TRUE, FALSE)
+  )
+  expect_equal(object = pairwise_test_bin(x = x, group = group, alternative = 'greater',
+                                          sorted_group = c('b','a')),
+               expected = testing_results)
+
+
+
+  # High number needed for testing
+  test_pasting <- paste_tbl_grp(data = testing_stats)
+  names(test_pasting) <- c('Comparison', 'ResponseStats')
+  testing_results <- data.frame(
+    test_pasting,
+    ResponseTest  = NA_integer_,
+    PerfectSeperation = ifelse(all(testing_stats_pre$mean == 0) | all(testing_stats_pre$mean == 1),
+                               TRUE, FALSE)
+  )
+  expect_equal(object = pairwise_test_bin(x = x, group = group, num_needed_for_test = 100),
+               expected = testing_results)
+
+
+  # Paired data (mcnemar testing)
+  paired_ids <- na.omit(test_data)$id[(duplicated(na.omit(test_data)$id))]
+  paired_stats_pre <- test_data %>%
+    filter(id %in% paired_ids) %>%
+    group_by(group) %>%
+    group_modify( ~ wilson_ci(.$x, .95)) %>%
+    mutate(rr = stat_paste(mean * 100, lower * 100, upper * 100, digits = 1, suffix = '%'))
+
+  paired_stats <- bind_cols(
+    paired_stats_pre %>% filter(group == 'a') %>% select(Group1 = group, Group1_rr = rr),
+    paired_stats_pre %>% filter(group == 'b') %>% select(Group2 = group, Group2_rr = rr)
+  )
+
+
+  test_pasting <- paste_tbl_grp(data = paired_stats)
+  names(test_pasting) <- c('Comparison', 'ResponseStats')
+  testing_results <- data.frame(
+    test_pasting,
+    ResponseTest  = two_samp_bin_test(x = x, y = group, method = 'mcnemar'),
+    PerfectSeperation = ifelse(diff(testing_stats_pre$mean) == 1, TRUE, FALSE)
+  )
+  expect_equal(object = pairwise_test_bin(x = x, group = group, id = id, method = 'mcnemar'),
+               expected = testing_results)
+
+
+})
+
+
+# test pairwise_test_bin with 3+ groups.
+# Using paste_tbl_grp and two_samp_cont_test in testing since these functions are testing elsewhere
+test_that("pairwise_test_bin testing 3+ groups", {
+
+  test_single_comp <- function(x, group, Group1, Group2) {
+    test_data <- tibble(x, group) %>% filter(group %in% c(Group1, Group2))
+
+    testing_stats_pre <- test_data %>%
+      filter(!is.na(x)) %>%
+      group_by(group) %>%
+      group_modify( ~ wilson_ci(.$x, .95)) %>%
+      mutate(rr = stat_paste(mean * 100, lower * 100, upper * 100, digits = 1, suffix = '%'))
+
+    testing_stats <- bind_cols(
+      testing_stats_pre %>% filter(group == Group1) %>% select(Group1 = group, Group1_rr = rr),
+      testing_stats_pre %>% filter(group == Group2) %>% select(Group2 = group, Group2_rr = rr)
+    )
+
+    test_pasting <- paste_tbl_grp(data = testing_stats)
+    names(test_pasting) <- c('Comparison', 'ResponseStats')
+
+    data.frame(
+      test_pasting,
+      ResponseTest  = two_samp_bin_test(x = test_data$x, y = test_data$group),
+      PerfectSeperation = ifelse(diff(testing_stats_pre$mean) == 1, TRUE, FALSE)
+    )
+  }
+
+  test_all_comp <- function(x, group){
+    x_here <- x[!is.na(x)]
+    group_here <- droplevels(group[!is.na(x)])
+    if (length(unique(group[!is.na(x)])) > 1) {
+      test_results_paste <- list()
+      for (i in 1:(nlevels(group_here) - 1)) {
+        for (j in ((i + 1):nlevels(group_here))) {
+          test_results_paste[[length(test_results_paste) + 1]] <-
+            test_single_comp(x = x_here, group = group_here,
+                             Group1 = levels(group_here)[i], Group2 = levels(group_here)[j])
+        }
+      }
+      do.call(base::rbind, test_results_paste)
+    }
+  }
+
+  testing_results <- testData_BAMA %>%
+    group_by(antigen, visit) %>%
+    group_modify(~ test_all_comp(.x$response, .x$group))
+
+  function_obj <- testData_BAMA %>%
+    group_by(antigen, visit) %>%
+    group_modify(~ pairwise_test_bin(.x$response, .x$group, num_needed_for_test = 2))
+
+  expect_equal(object = function_obj,
+               expected = testing_results)
+
+})
+
