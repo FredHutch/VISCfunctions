6ece9a60201b20b29fbeff83a870b95ada023f0a
Dave Slager <slager@users.noreply.github.com>
2025-12-05 16:44:35 -0800

    report attached and loaded packages in reproducibility table

--- tests/testthat/test_reproducibility_tables.R
+++ tests/testthat/test_reproducibility_tables.R
@@ -20,14 +20,17 @@ test_that("get_session_info() testing", {
     ifelse(any(temp_session_info$platform_table$name == "repo"), 5, 4)
   expect_equal(object = dim(temp_session_info$platform_table), expected = c(nrow_expected,2))
 
-  ncol_expected <- ifelse(any(colnames(temp_session_info$packages_table) == "data.version"), 5, 4)
+  ncol_expected <- ifelse(any(colnames(temp_session_info$packages_table) == "data.version"), 6, 5)
   expect_equal(object = ncol(temp_session_info$packages_table), expected = ncol_expected)
 
   # test libpath column option produces that column
   expect_true('libpath' %in% names(get_session_info(libpath = TRUE)$packages_table))
 
   ## testing some outputs from sessioninfo::session_info()
-  expected_session_info <- sessioninfo::session_info()
+  expected_session_info <- sessioninfo::session_info(pkgs = 'loaded')
+
+  expected_session_info$packages$status <-
+    ifelse(expected_session_info$packages$attached, 'attached', 'loaded')
 
   # Comparing platform
   expected_platform <- data.frame(
@@ -37,11 +40,12 @@ test_that("get_session_info() testing", {
   expect_equal(object = temp_session_info$platform_table[match(expected_platform$name, temp_session_info$platform_table$name), ], expected = expected_platform)
 
   # Comparing packages
-  expected_packages <- expected_session_info$packages[expected_session_info$packages$attached,]
+  expected_packages <- expected_session_info$packages[order(expected_session_info$packages$status),]
   expected_packages <- data.frame(package = expected_packages$package,
                                  version = expected_packages$loadedversion,
                                  date = expected_packages$date,
                                  source = shorten_git_hash(expected_packages$source),
+                                 status = expected_packages$status,
                                  stringsAsFactors = FALSE)
 
   expect_equal(object = temp_session_info$packages_table, expected = expected_packages)
